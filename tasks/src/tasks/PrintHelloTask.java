41104531e5f4242a668eab0c637b28924274c52e
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2024-08-04 16:40:04 -0300

    Getting Started with Threads - Creating tasks

---
+++ tasks/src/tasks/PrintHelloTask.java
@@ -0,0 +1,22 @@
+package tasks;
+
+public class PrintHelloTask implements Runnable {
+	
+	private String name;
+	
+	public PrintHelloTask(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(60000);
+		} catch (InterruptedException e) {
+			System.out.println(String.format("%s foi interrompida!", this.name));
+			e.printStackTrace();
+		}
+		System.out.println(String.format("Olá, %s!", this.name));
+	}
+
+}
